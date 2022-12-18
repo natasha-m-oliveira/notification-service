b95a0e54a7ba5d2bb28e7effeb8a793189a50bba
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-12-18 15:08:37 -0300

    feat: connect with kafka

---
+++ src/infra/messaging/kafka/kafka-consumer.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+import { ServerKafka } from '@nestjs/microservices';
+
+@Injectable()
+export class KafkaConsumerService
+  extends ServerKafka
+  implements OnModuleDestroy
+{
+  constructor() {
+    super({
+      client: {
+        clientId: 'notifications',
+        brokers: ['novel-ram-10802-us1-kafka.upstash.io:9092'],
+        sasl: {
+          mechanism: 'scram-sha-256',
+          username: process.env.KAFKA_USERNAME as string,
+          password: process.env.KAFKA_PASSWORD as string,
+        },
+        ssl: true,
+      },
+    });
+  }
+
+  async onModuleDestroy() {
+    await this.close();
+  }
+}
