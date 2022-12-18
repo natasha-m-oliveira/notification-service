f0f0b1dbc9c4d6dc5214968975756a1f2865c5eb
Natasha Matos <natasha.oliveirabusiness@gmail.com>
2022-12-17 21:51:02 -0300

    feat: cancel sending notification

---
+++ src/app/use-cases/cancel-notification.spec.ts
@@ -0,0 +1,34 @@
+import { makeNotification } from '@test/factories/notification-factory';
+import { InMemoryNotificationsRepository } from '@test/repositories/in-memory-notifications-repository';
+import { CancelNotification } from './cancel-notification';
+import { NotificationNotFound } from './errors/notification-not-found';
+
+let notificationsRepository: InMemoryNotificationsRepository;
+let cancelNotification: CancelNotification;
+describe('Cancel notification', () => {
+  beforeEach(() => {
+    notificationsRepository = new InMemoryNotificationsRepository();
+    cancelNotification = new CancelNotification(notificationsRepository);
+  });
+  it('should be able to cancel a notification', async () => {
+    const notification = makeNotification();
+
+    await notificationsRepository.create(notification);
+
+    await cancelNotification.execute({
+      notificationId: notification.id,
+    });
+
+    expect(notificationsRepository.notifications[0].canceledAt).toEqual(
+      expect.any(Date),
+    );
+  });
+
+  it('should not be able to cancel a non existing notification', () => {
+    expect(() => {
+      return cancelNotification.execute({
+        notificationId: 'fake-notification-id',
+      });
+    }).rejects.toThrow(NotificationNotFound);
+  });
+});
